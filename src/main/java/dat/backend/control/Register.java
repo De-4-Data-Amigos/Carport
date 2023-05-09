0c6e63e6ccc4ed5b7a69541517b55985ae94c085
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-05-09 18:01:53 +0200

    register updt.

--- src/main/java/dat/backend/control/Register.java
+++ src/main/java/dat/backend/control/Register.java
@@ -24,6 +24,9 @@ public class Register extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
+
+
     }
 
     @Override
