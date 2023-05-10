967004a2c01646fb6cf07b60b49299b0160a47cb
Kussemar <marcus.dofinsmurt@gmail.com>
2023-05-10 12:54:17 +0200

    admin WIP

--- src/main/java/dat/backend/control/Admin.java
+++ src/main/java/dat/backend/control/Admin.java
@@ -1,19 +1,65 @@
 package dat.backend.control;
 
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.AdminFacade;
+import dat.backend.model.persistence.ConnectionPool;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "Admin", value = "/Admin")
+@WebServlet(name = "Admin", value = "/admin")
 public class Admin extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException {
+        this.connectionPool = ApplicationStart.getConnectionPool();
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null) {
+            if (user.getRole().equalsIgnoreCase("admin")) {
+                List<User> users = null;
+                List<Order> orders = null;
+                try {
+                    users = AdminFacade.getAllUsers(connectionPool);
 
+                } catch (DatabaseException e)
+                {
+                    request.setAttribute("errormessage", e.getMessage());
+                    request.getRequestDispatcher("error.jsp").forward(request, response);
+                }
+                request.setAttribute("userList", users);
+                request.setAttribute("orderList",orders);
+                request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request, response);
+
+            }
+        }
+
+        request.setAttribute("besked", "du er ikke en admin");
+        request.getRequestDispatcher("index").forward(request, response);
+
+    }
+
+}
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+
+            request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request, response);
+
+        } catch (DatabaseException e) {
+            request.setAttribute("errormessage", e.getMessage());
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
     }
 }
--- src/main/java/dat/backend/model/persistence/AdminMapper.java
+++ src/main/java/dat/backend/model/persistence/AdminMapper.java
@@ -1,4 +1,43 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminMapper {
+    static List<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT * FROM carport.user;";
+
+        List<User> userList = new ArrayList<>();
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int userId = rs.getInt("userid");
+                    String email= rs.getString("email");
+                    String password = rs.getString("password");
+                    String firstname = rs.getString("firstname");
+                    String lastname = rs.getString("lastname");
+                    int phoneNumber = rs.getInt("phonenumber");
+                    String role = rs.getString("role");
+
+
+
+
+                    User user = new User(userId, email, password, firstname, lastname, phoneNumber, role);
+                    userList.add(user);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e, "Fejl i tilgangen til databasen");
+        }
+        return userList;
+    }
 }
