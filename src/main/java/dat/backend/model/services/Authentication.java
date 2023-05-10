8e0fafa8b57838c98c003a1e41dafa9d1dcc68b0
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-05-10 12:54:51 +0200

    carportBuilder
    added Helper Class

--- src/main/java/dat/backend/model/services/Authentication.java
+++ src/main/java/dat/backend/model/services/Authentication.java
@@ -16,4 +16,6 @@ public class Authentication
         }
         return false;
     }
+
+
 }
---
+++ src/main/java/dat/backend/model/services/CarportBuilderHelper.java
@@ -0,0 +1,18 @@
+package dat.backend.model.services;
+
+import dat.backend.control.Order;
+import dat.backend.model.entities.ItemEntry;
+
+import java.util.List;
+
+public class CarportBuilderHelper {
+
+//TODO her skal vi generate vores stykliste.
+    public static List<ItemEntry> generateItemList(int width, int length, Order order) {
+
+    }
+
+    public static float startPrice(){
+        return 0;
+    }
+}
