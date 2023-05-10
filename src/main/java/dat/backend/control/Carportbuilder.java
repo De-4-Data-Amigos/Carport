739dac1c433ff235c5fc2eebcf1c1907525dfa10
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-05-10 12:54:51 +0200

    carportBuilder
    added Helper Class

---
+++ src/main/java/dat/backend/control/Carportbuilder.java
@@ -0,0 +1,38 @@
+package dat.backend.control;
+
+import dat.backend.model.entities.ItemEntry;
+import dat.backend.model.entities.User;
+import dat.backend.model.services.CarportBuilderHelper;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Carportbuilder", value = "/carportbuilder")
+public class Carportbuilder extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("checkout").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String widthString = request.getParameter("width");
+        String lengthString = request.getParameter("length");
+
+        int width = Integer.parseInt(widthString);
+        int length = Integer.parseInt(lengthString);
+        User user = (User) request.getSession().getAttribute("user");
+        Order order = new Order(width, length, user);
+        List<ItemEntry> itemEntryList = CarportBuilderHelper.generateItemList(width, length, order);
+        request.setAttribute("itemList", itemEntryList);
+        request.getRequestDispatcher("checkout").forward(request,response);
+
+
+
+    }
+}
