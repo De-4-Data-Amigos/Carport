d08d5175fa2de1516f31382306fdc5d3c2c43675
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-05-11 12:57:24 +0200

    NY BRANCH TJEKOUT :)
    hæh med jsp side jaja

--- src/main/java/dat/backend/control/Checkout.java
+++ src/main/java/dat/backend/control/Checkout.java
@@ -36,6 +36,7 @@ public class Checkout extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         request.getRequestDispatcher("checkout.jsp").forward(request, response);
     }
 }
