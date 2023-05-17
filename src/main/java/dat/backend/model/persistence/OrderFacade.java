922941b6738e3a7172bd94d1625d72b5075e934b
Kussemar <marcus.dofinsmurt@gmail.com>
2023-05-17 09:56:17 +0200

    nu er det muligt at fjerne fra productlist

--- src/main/java/dat/backend/model/persistence/OrderFacade.java
+++ src/main/java/dat/backend/model/persistence/OrderFacade.java
@@ -23,4 +23,5 @@ public class OrderFacade {
         OrderMapper.removeOrder(id, connectionPool);
     }
 
+
 }
--- src/main/java/dat/backend/model/persistence/ProductFacade.java
+++ src/main/java/dat/backend/model/persistence/ProductFacade.java
@@ -9,4 +9,12 @@ public class ProductFacade {
     public static List<Product> getAllProducts(ConnectionPool connectionPool) throws DatabaseException {
         return ProductMapper.getAllProducts(connectionPool);
     }
+
+    public static void removeProductList(int id, ConnectionPool connectionPool) throws DatabaseException {
+        ProductMapper.removeProductList(id, connectionPool);
+    }
+
+    public static void removeProductVariant(int id, ConnectionPool connectionPool) throws DatabaseException {
+        ProductMapper.removeProductVariant(id, connectionPool);
+    }
 }
