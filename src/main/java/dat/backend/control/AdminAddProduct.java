6ccdec20d4d7b0349915d05bd4b36248cdaa3482
Kussemar <marcus.dofinsmurt@gmail.com>
2023-05-17 11:17:30 +0200

    admin add product wip

---
+++ src/main/java/dat/backend/control/AdminAddProduct.java
@@ -0,0 +1,30 @@
+package dat.backend.control;
+
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.persistence.ConnectionPool;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AdminAddProduct", value = "/AdminAddProduct")
+public class AdminAddProduct extends HttpServlet {
+
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
