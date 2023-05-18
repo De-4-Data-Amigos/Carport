0ecb468c6be0d9e7488eb72fa1f6cf1ebec6b4c5
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-05-18 11:04:29 +0200

    edits

--- src/main/java/dat/backend/control/EditAdminProduct.java
+++ src/main/java/dat/backend/control/EditAdminProduct.java
@@ -26,7 +26,17 @@ public class EditAdminProduct extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        int Id = Integer.parseInt(request.getParameter("id"));
+        int productId = Integer.parseInt(request.getParameter("product_id"));
+        int pricePerUnit = Integer.parseInt(request.getParameter("price_per_unit"));
+        int width = Integer.parseInt(request.getParameter("width"));
+        int length = Integer.parseInt(request.getParameter("length"));
 
+        int phonenumber = Integer.parseInt(request.getParameter("phonenumber"));
+
+        String newDescription = request.getHeader("description")
+        String newName = request.getParameter("namename");
+        String newUnit = request.getParameter("unit");
 
 
         request.sendRedirect("editadminproduct.jsp").forward(request, response);
